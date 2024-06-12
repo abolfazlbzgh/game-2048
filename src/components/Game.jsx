6a134242b5f20705c1295fe86dee787e4a58600a
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-06-12 15:08:49 -0500

    debug win modal

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -68,7 +68,7 @@ export default function Game() {
 
     useEffect(() => {
         if (checkWin() && !showWinModal) {
-            setGameWon(true);
+            setHasWon(true);
             setShowWinModal(true);
         }
     }, [board, showWinModal]);
